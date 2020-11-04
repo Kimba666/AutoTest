34b2363e21f4eed65dffadf37181b907dc27f477
Kimba <jwolflive@gmail.com>
2020-11-04 13:36:35 -0500

    test

--- cypress/integration/tests/sample_spec.js
+++ cypress/integration/tests/sample_spec.js
@@ -1,11 +1,15 @@
-describe('Google Image Turkish Landscape', () => {
+describe('[TEST-T1]', () => {
+
     it('Automation for Jira [TEST-T1]', () => {
+
         cy.visit('https://www.google.com/')
 
         cy.get('#tsf > div:nth-child(2) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input').click()
             .type('Turkish Landscape')
             .should('have.value', 'Turkish Landscape')
             .type('{enter}')
+
         cy.get('#hdtb-msb-vis > div:nth-child(2) > a').click()
+
     })
 })
