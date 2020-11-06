0e4ac23da1a0cf6ecc287e7dd543c09741301454
Kimba <jwolflive@gmail.com>
2020-11-06 15:02:19 -0500

    test

--- cypress/integration/tests/TEST-T1.js
+++ cypress/integration/tests/TEST-T1.js
@@ -1,17 +1,19 @@
 describe('TEST-T1', () => {
 
-    it('TEST-R1', options => {
+    it('TEST-R1', () => {
 
         // Navigate to google
-        cy.visit('https://www.google.com/')
+        cy.visit('https://www.google.com/');
 
         // Click for Turkish Landscape
         cy.get('#tsf > div:nth-child(2) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input').click()
             .type('Turkish Landscape')
             .should('have.value', 'Turkish Landscape')
-            .type('{enter}')
+            .type('{enter}');
 
         // Click on images
-        cy.get('#hdtb-msb-vis > div:nth-child(2) > a').click()
+        cy.get('#hdtb-msb-vis > div:nth-child(2) > a')
+            .should('contain', 'Images')
+            .click()
     })
 })
