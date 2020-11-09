c7cd777f5a7013c956d497ce87ffc48cdb74c0c5
Kimba666 <jwolflive@gmail.com>
2020-11-09 11:20:57 -0500

    test

--- cypress/integration/tests/TEST-T2.js
+++ cypress/integration/tests/TEST-T2.js
@@ -1,19 +1,18 @@
 describe('TEST-T2', () => {
 
     it('TEST-R2', () => {
-
-            // Navigate to google
-            cy.visit('https://www.google.com/')
+        // Navigate to google
+        cy.visit('https://www.google.com/')
 
             // Search for Kittens
-            cy.get('#tsf > div:nth-child(2) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input').click()
-                .type('Kittens')
-                .should('have.value', 'Kittens')
-                .type('{enter}')
+        cy.get('#tsf > div:nth-child(2) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input').click()
+            .type('Kittens')
+            .should('have.value', 'Kittens')
+            .type('{enter}')
 
             // Click on images
-            cy.get('#hdtb-msb-vis > div:nth-child(2) > a')
-                .should('contain', 'Images')
-                .click()
+        cy.get('#hdtb-msb-vis > div:nth-child(2) > a')
+            .should('contain', 'Images')
+            .click()
         })
 })
