326a7b7f888af5c0479f5c86a4561c0874eec4fd
Kimba <jwolflive@gmail.com>
2020-11-11 10:43:23 -0500

    test

--- cypress/api/jira-api-put.js
+++ cypress/api/jira-api-put.js
@@ -1,33 +1,33 @@
-var glob = require("glob")
+const glob = require("glob")
+const path = require('path')
 const fetch = require("node-fetch");
-const cypressTestResult = require('../results/json/mochawesome.json')
-const passOrFail = () => cypressTestResult.results[0].suites[0].tests[0].pass ? 'Pass' : 'Fail';
-const files = glob.sync(path.join(__dirname, 'cypress/results/tests/*.js'))
-files.forEach((file) => { require(file) })
+const cypressTestResults = glob.sync('../results/json/*.json')
+console.log(cypressTestResults)
+cypressTestResults.forEach((file) => {
+    const cypressTestResult = require(file)
+        const passOrFail = () => cypressTestResult.results[0].suites[0].tests[0].pass ? 'Pass' : 'Fail';
+        const testKey = () => cypressTestResult.results[0].suites[0].title;
+    console.log(cypressTestResult.results[0].suites[0].title);
+    console.log(passOrFail())
 
+    // Post to Jira TM4J endpoint with results
+    fetch('https://api.adaptavist.io/tm4j/v2/testexecutions', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIwY2E0Njk0OS04NGFjLTMyMzktYjVjOC05ZTE1NzNiMTVkNGUiLCJjb250ZXh0Ijp7ImJhc2VVcmwiOiJodHRwczpcL1wvdGVhbWdsb2JhbHJpc2suYXRsYXNzaWFuLm5ldCIsInVzZXIiOnsiYWNjb3VudElkIjoiNWQzYjRkNzkwZjQ3OGIwYzFiOGU2YjYxIn19LCJpc3MiOiJjb20ua2Fub2FoLnRlc3QtbWFuYWdlciIsImV4cCI6MTYzNjA2MTQ5NywiaWF0IjoxNjA0NTI1NDk3fQ.m0RPKzCRd28_t3QokE_IWC4Wh3mxQthYUE3sOyplTAY'
+        },
+        body: JSON.stringify({
+            projectKey: 'TEST',
+            testCaseKey: cypressTestResult.results[0].suites[0].title,
+            testCycleKey: cypressTestResult.results[0].suites[0].tests[0].title,
+            statusName: passOrFail(),
+            executionTime: cypressTestResult.results[0].suites[0].title[0].duration,
+            executedById: '5d3b4d790f478b0c1b8e6b61',
+            comment: `${testKey()}: ${passOrFail()}`
+        })
 
-
-    // Display Test name and result verdict
-console.log(cypressTestResult.results[0].suites[0].title);
-console.log(passOrFail())
-
-
-
-    // Hit Jira TM4J API endpoint to update test execution
-fetch('https://api.adaptavist.io/tm4j/v2/testexecutions', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIwY2E0Njk0OS04NGFjLTMyMzktYjVjOC05ZTE1NzNiMTVkNGUiLCJjb250ZXh0Ijp7ImJhc2VVcmwiOiJodHRwczpcL1wvdGVhbWdsb2JhbHJpc2suYXRsYXNzaWFuLm5ldCIsInVzZXIiOnsiYWNjb3VudElkIjoiNWQzYjRkNzkwZjQ3OGIwYzFiOGU2YjYxIn19LCJpc3MiOiJjb20ua2Fub2FoLnRlc3QtbWFuYWdlciIsImV4cCI6MTYzNjA2MTQ5NywiaWF0IjoxNjA0NTI1NDk3fQ.m0RPKzCRd28_t3QokE_IWC4Wh3mxQthYUE3sOyplTAY'
-    },
-    body: JSON.stringify({
-        projectKey: 'TEST',
-        testCaseKey: cypressTestResult.results[0].suites[0].title,
-        testCycleKey: cypressTestResult.results[0].suites[0].tests[0].title,
-        statusName: passOrFail()
-    })
-
-
-
-    // Log data or error post run
-}).then(res => res.json()).then(data => console.log(data)).catch(error => console.log(error))
+        // Log data or error post run
+    }).then(res => res.json()).then(data => console.log(data)).catch(error => console.log(error))
+    }
+)
