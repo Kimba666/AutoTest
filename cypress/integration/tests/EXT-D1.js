bdb24ca4c8864b38849048a9638526d4aca8c028
Kimba666 <jwolflive@gmail.com>
2020-11-12 10:02:05 -0500

    test

--- cypress/integration/tests/EXT-D1.js
+++ cypress/integration/tests/EXT-D1.js
@@ -13,7 +13,7 @@ describe('EXT-D1', () => {
 
         //Click on Images to view images
         cy.get('#hdtb-msb-vis > div:nth-child(2) > a')
-            .should('have.value', 'Images')
+            .should('contain', 'Images')
             .click()
     })
 })
