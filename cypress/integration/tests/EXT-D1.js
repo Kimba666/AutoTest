5ccdc09d8dbaf3ce0e5bf30284b793c5b9e68102
Kimba666 <jwolflive@gmail.com>
2020-11-12 09:55:23 -0500

    test

---
+++ cypress/integration/tests/EXT-D1.js
@@ -0,0 +1,19 @@
+describe('EXT-D1', () => {
+
+    it('EXT-R1', () => {
+
+        //Navigate to Google
+        cy.get('https://www.google.com/');
+
+        //Click search field and enter value
+        cy.get('#tsf > div:nth-child(2) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input').click()
+            .type('Tiger cub')
+            .should('have.value', 'Tiger cub')
+            .type('{enter}');
+
+        //Click on Images to view images
+        cy.get('#hdtb-msb-vis > div:nth-child(2) > a')
+            .should('have.value', 'Images')
+            .click()
+    })
+})
